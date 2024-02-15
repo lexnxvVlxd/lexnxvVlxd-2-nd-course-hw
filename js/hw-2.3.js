74c15f38fd085752f2e4d013736816e55eeaa623
Vlad <errxrcxde@mail.ru>
2024-02-15 13:23:29 +0300

    Completed first JS assignment.

---
+++ js/hw-2.3.js
@@ -0,0 +1,5 @@
+// Задание 1
+
+let password = 'пароль'
+let enterPassword = prompt('Введите пароль')
+let passwordEntryResult = (enterPassword === password) ? console.log('Пароль введен верно') : console.log('Пароль введен неправильно')
