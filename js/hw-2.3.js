af2bdd35d2e0de8353b4c7e28463a02c8e2743a3
Vlad <errxrcxde@mail.ru>
2024-02-15 13:35:19 +0300

    Completed second task in JS.

--- js/hw-2.3.js
+++ js/hw-2.3.js
@@ -1,5 +1,10 @@
 // Задание 1
 
-let password = 'пароль'
-let enterPassword = prompt('Введите пароль')
-let passwordEntryResult = (enterPassword === password) ? console.log('Пароль введен верно') : console.log('Пароль введен неправильно')
+// let password = 'пароль'
+// let enterPassword = prompt('Введите пароль')
+// let passwordEntryResult = (enterPassword === password) ? console.log('Пароль введен верно') : console.log('Пароль введен неправильно')
+
+// Задание 2
+
+let c = prompt('Введите число от 0 до 10')
+let resultC = (c > 0 && c < 10) ? console.log('Верно') : console.log('Не верно')
