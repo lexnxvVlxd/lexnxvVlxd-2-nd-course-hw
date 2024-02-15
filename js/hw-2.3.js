793d091cf01389a7c5fe3f8ae6fe37fcb26baf59
Vlad <errxrcxde@mail.ru>
2024-02-15 13:54:13 +0300

    Сompleted fourth task in JS.

--- js/hw-2.3.js
+++ js/hw-2.3.js
@@ -13,12 +13,19 @@
 
 // Условие где одна из переменных больше 100
 
-let d = 455
-let e = 11
+// let d = 455
+// let e = 11
 
 // Условие где переменные меньше 100
 
 // d = 45
 // e = 11
 
-let resultTask3 = (d > 100 || e > 100) ? console.log('Верно') : console.log('Не верно')
+// let resultTask3 = (d > 100 || e > 100) ? console.log('Верно') : console.log('Не верно')
+
+// Задание 4
+
+let a = '2'
+let b = '3'
+
+alert(Number(a) + Number(b))
