8c5f7bdc689ce5e5a8dc7d7225491dc7488b5b1e
Vlad <errxrcxde@mail.ru>
2024-02-15 13:49:55 +0300

    Сompleted third task in JS.

--- js/hw-2.3.js
+++ js/hw-2.3.js
@@ -6,5 +6,19 @@
 
 // Задание 2
 
-let c = prompt('Введите число от 0 до 10')
-let resultC = (c > 0 && c < 10) ? console.log('Верно') : console.log('Не верно')
+// let c = prompt('Введите число от 0 до 10')
+// let resultTask2 = (c > 0 && c < 10) ? console.log('Верно') : console.log('Не верно')
+
+// Задание 3
+
+// Условие где одна из переменных больше 100
+
+let d = 455
+let e = 11
+
+// Условие где переменные меньше 100
+
+// d = 45
+// e = 11
+
+let resultTask3 = (d > 100 || e > 100) ? console.log('Верно') : console.log('Не верно')
