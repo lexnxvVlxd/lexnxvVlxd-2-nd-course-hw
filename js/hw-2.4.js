2851c8c5fa682390ce068eccaa8095fe6fa20384
Vlad <errxrcxde@mail.ru>
2024-02-20 11:21:35 +0300

    finished task №2

--- js/hw-2.4.js
+++ js/hw-2.4.js
@@ -1,6 +1,13 @@
 // Задание 1
 
-for (let i = 2; i > 0; i--) {
-  console.log('Привет')
-}
+// for (let i = 2; i > 0; i--) {
+//   console.log('Привет')
+// }
 
+// Задание 2
+
+const number = 5
+
+for (let i = 1; i <= number; i++) {
+  console.log(i)
+}
