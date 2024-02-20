f893b19141708eb7d54d4578b7d550397edb41cb
Vlad <errxrcxde@mail.ru>
2024-02-20 11:43:57 +0300

    finished task №4

--- js/hw-2.4.js
+++ js/hw-2.4.js
@@ -14,8 +14,20 @@
 
 // Задание 3
 
-const number = 22
+// const number = 22
 
-for (let i = 7; i <= number; i++) {
-  console.log(i)
+// for (let i = 7; i <= number; i++) {
+//   console.log(i)
+// }
+
+// Задание 4
+
+let obj = {
+  'Коля': '200',
+  'Вася': '300',
+  'Петя': '400'
+}
+
+for (let key in obj) {
+  console.log(`"${key}" - зарплата "${obj[key]}" долларов`)
 }
