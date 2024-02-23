b4e896308a5dfa772ffd648818fae803db6372a7
Vlad <errxrcxde@mail.ru>
2024-02-23 10:51:40 +0300

    finished task №4

--- js/hw-2.5.js
+++ js/hw-2.5.js
@@ -12,6 +12,24 @@
 
 //task №3
 
-const squaresNumber = (number) => {
-  return number * number
+// const squaresNumber = (number) => {
+//   return number * number
+// }
+
+//task №4 
+
+const checksAge = () => {
+  let userAge = prompt('Сколько вам лет?')
+
+  if (userAge < 0) {
+    return 'Вы ввели неправильное значение'
+  } else if (userAge === null) {
+    return
+  } else if (userAge >= 0 && userAge <= 12) {
+    return 'Привет, друг!'
+  } else if (userAge >= 13) {
+    return 'Добро пожаловать!'
+  }
 }
+
+console.log(checksAge())
