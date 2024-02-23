29a7e83b6344b57934ad4cc55c058945619cb09b
Vlad <errxrcxde@mail.ru>
2024-02-23 11:48:46 +0300

    finished task №5

--- js/hw-2.5.js
+++ js/hw-2.5.js
@@ -18,18 +18,25 @@
 
 //task №4 
 
-const checksAge = () => {
-  let userAge = prompt('Сколько вам лет?')
+// const checksAge = () => {
+//   let userAge = prompt('Сколько вам лет?')
 
-  if (userAge < 0) {
-    return 'Вы ввели неправильное значение'
-  } else if (userAge === null) {
-    return
-  } else if (userAge >= 0 && userAge <= 12) {
-    return 'Привет, друг!'
-  } else if (userAge >= 13) {
-    return 'Добро пожаловать!'
-  }
+//   if (userAge < 0) {
+//     return 'Вы ввели неправильное значение'
+//   } else if (userAge === null) {
+//     return
+//   } else if (userAge >= 0 && userAge <= 12) {
+//     return 'Привет, друг!'
+//   } else if (userAge >= 13) {
+//     return 'Добро пожаловать!'
+//   }
+// }
+
+// console.log(checksAge())
+
+//task №5
+
+const multiplyNumbers = (num1, num2) => {
+  return !Number.isNaN(num1 * num2) ? 
+  num1 * num2 : 'Одно или оба значения не являются числом';
 }
-
-console.log(checksAge())
