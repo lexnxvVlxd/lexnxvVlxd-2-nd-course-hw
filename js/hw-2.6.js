aa4625306e0e6e58cb89be3eded54a99231fe3d3
Vlad <errxrcxde@mail.ru>
2024-03-01 10:45:43 +0300

    finished task №3

--- js/hw-2.6.js
+++ js/hw-2.6.js
@@ -13,10 +13,18 @@
 
 // task №2
 
-const arr = [1, 5, 4, 10, 0, 3]
+// const arr = [1, 5, 4, 10, 0, 3]
 
-arr.forEach((element, i) => {
-  if (element === 4) {
-    console.log(`Значение "${element}" нахоидтся в массиве под индексом: ${i}`)
-  }
-});
+// arr.forEach((element, i) => {
+//   if (element === 4) {
+//     console.log(`Значение "${element}" нахоидтся в массиве под индексом: ${i}`)
+//   }
+// });
+
+// task №3
+
+const arr = [1, 3, 5, 10, 20]
+
+let str = arr.join(' ')
+
+console.log(str)
