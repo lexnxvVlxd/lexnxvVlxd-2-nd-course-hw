30c5246709bbd4616901e3869110f3f6b59fdc55
Vlad <errxrcxde@mail.ru>
2024-03-01 10:42:07 +0300

    finished task №2

--- js/hw-2.6.js
+++ js/hw-2.6.js
@@ -1,12 +1,22 @@
 // task №1
 
+// const arr = [1, 5, 4, 10, 0, 3]
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(`Индекс массива: ${i} его значение ${arr[i]}`)
+
+//   if (arr[i] === 10) {
+//     console.log('Цикл остановлен в массиве есть элемент со значением "10"')
+//     break 
+//   }
+// }
+
+// task №2
+
 const arr = [1, 5, 4, 10, 0, 3]
 
-for (let i = 0; i < arr.length; i++) {
-  console.log(`Индекс массива: ${i} его значение ${arr[i]}`)
-
-  if (arr[i] === 10) {
-    console.log('Цикл остановлен в массиве есть элемент со значением "10"')
-    break 
+arr.forEach((element, i) => {
+  if (element === 4) {
+    console.log(`Значение "${element}" нахоидтся в массиве под индексом: ${i}`)
   }
-}
+});
