778a8aa11fa37918e35426646ab5fc991297ea79
Vlad <errxrcxde@mail.ru>
2024-03-08 15:52:10 +0300

    finished task №2

--- js/hw-2.7.js
+++ js/hw-2.7.js
@@ -1,5 +1,24 @@
 // task №1
 
-let str = 'js'
+// let str = 'js'
 
-console.log(str.toUpperCase())
+// console.log(str.toUpperCase())
+
+// task №2
+
+let arr = ['Two One', 'One One', 'One Two', 'Two Two']
+let keyword = 'One'
+
+const elementsWithSecondRow = (arr, str) => {
+  let newArr = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].toLowerCase().endsWith(str.toLowerCase())) {
+      newArr.push(arr[i])
+    }
+  }
+
+  return newArr
+}
+
+console.log(elementsWithSecondRow(arr, keyword))
