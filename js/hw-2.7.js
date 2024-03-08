f968db1f40ded0ffe747d43ee0af97dfdfce4501
Vlad <errxrcxde@mail.ru>
2024-03-08 16:46:22 +0300

    finished task №3

--- js/hw-2.7.js
+++ js/hw-2.7.js
@@ -6,19 +6,28 @@
 
 // task №2
 
-let arr = ['Two One', 'One One', 'One Two', 'Two Two']
-let keyword = 'One'
+// let arr = ['Two One', 'One One', 'One Two', 'Two Two']
+// let keyword = 'One'
 
-const elementsWithSecondRow = (arr, str) => {
-  let newArr = []
+// const elementsWithSecondRow = (arr, str) => {
+//   let newArr = []
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].toLowerCase().endsWith(str.toLowerCase())) {
-      newArr.push(arr[i])
-    }
-  }
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].toLowerCase().endsWith(str.toLowerCase())) {
+//       newArr.push(arr[i])
+//     }
+//   }
 
-  return newArr
-}
+//   return newArr
+// }
 
-console.log(elementsWithSecondRow(arr, keyword))
+// console.log(elementsWithSecondRow(arr, keyword))
+
+// task №3
+
+let num = 32.58884
+
+console.log(`Округлённое число до меньшего ${Math.floor(num)}, 
+до большего ${Math.ceil(num)}, 
+до ближайшего ${Math.round(num)}`
+)
