eeb0543e2c036ebdb42145685b83120e4ffac879
Vlad <errxrcxde@mail.ru>
2024-03-14 14:55:26 +0300

    finished task №1

---
+++ js/hw-2.8.js
@@ -0,0 +1,12 @@
+// task №1
+
+// const people = [
+//   { name: 'Глеб', age: 29 },
+//   { name: 'Анна', age: 17 },
+//   { name: 'Олег', age: 7 },
+//   { name: 'Оксана', age: 47 }
+// ];
+
+// people.sort((a, b) => a.age - b.age)
+
+// console.log(people)
